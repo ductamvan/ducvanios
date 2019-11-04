86a8081940e8b402bb03d67976ab3a4890fe19f6
Duc Van <duc.van@student.hogent.be>
2019-11-04 14:15:43 +0100

    Initial Commit

---
+++ NativeAppsFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NativeAppsFinalProject
+//
+//  Created by Duc Van on 04/11/2019.
+//  Copyright © 2019 Duc Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
